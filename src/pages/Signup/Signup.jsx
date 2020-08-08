17b6dadc93b760ffafdcebc0ff2366f661c7f768
Albatrooss <robillardtim@gmail.com>
2020-08-08 12:00:11 -0400

    fixed login

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -18,6 +18,14 @@ class Signup extends Component {
 
   handleSubmit = async e => {
     e.preventDefault();
+    if (this.state.password !== this.state.passwordConf) {
+      return this.setState(state => {
+        return {
+          ...state,
+          errMsg: 'Passwords don\'t match..'
+        }
+      })
+    }
     try {
       await userService.signup(this.state);
       this.props.handleSignUp();
@@ -40,12 +48,16 @@ class Signup extends Component {
               <input onChange={this.handleChange} name="username" type="text" required />
             </div>
             <div className="input-field col s12 my-form-outline">
-              <div className="my-label">Username</div>
+              <div className="my-label">Email</div>
               <input onChange={this.handleChange} name="email" type="email" required />
             </div>
             <div className="input-field col s12 my-form-outline">
-              <div className="my-label">Username</div>
-              <input onChange={this.handleChange} name="pw" type="password" required />
+              <div className="my-label">Password</div>
+              <input onChange={this.handleChange} name="password" type="password" required />
+            </div>
+            <div className="input-field col s12 my-form-outline">
+              <div className="my-label">ConfrimPassword</div>
+              <input onChange={this.handleChange} name="passwordConf" type="password" required />
             </div>
             <p>Allready a member? <a href="/login">Login</a></p>
             <button className="btn " type="submit">SIGN UP</button>
