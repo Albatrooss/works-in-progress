cc9391b383616c7120d7f94119079755286c5287
Albatrooss <robillardtim@gmail.com>
2020-09-02 16:27:14 -0400

    fixed dir message wrapping

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,8 @@ import './Footer.css'
 export default function Footer({ user }) {
   return (
     <footer>
+      <h6>Have any questions or concerns? Send us an email and we'll get back to you as soon as possible!</h6>
+      <a href="mailto:hello@timrobillard.com?subject=FROM WORKS IN PROGRESS" target="_blank" className="btn">EMAIL US</a>
       <p>&copy; 2020 Tim Robillard</p>
     </footer>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,23 +14,21 @@ export default function Home() {
           <div className="col s4 cait-main-img">
             <img src="images/caitlin_elmslie_headshot.jpg" alt="" className='z-depth-1' />
           </div>
-          <div className="col s12 l8">
+          <div className="col s12 l6">
             <img src="images/caitlin_elmslie_headshot.jpg" alt="" className='z-depth-1 mobile-main-img' />
             <p>Caitlin is an aspiring clinician scientist with a graduate degree in dance and undergraduate degrees in kinesiology and psychology. She is currently in her final year of study in Occupational Therapy at the University of Toronto.</p>
             <p>Caitlin is a Pilates Mat Instructor certified through Body Harmonics. She has taught dance for 15 years for students of all ages from young children to adults in studios and for programs across Ontario. A highlight of her training was the 3 months Caitlin spent in New York City studying at Broadway Dance Center as part of their International Student Visa Program. </p>
           </div>
         </div>
-        <div>
-          <div className="col s12 l6 offset-l3">
-            <p>The goal of Works in Progress Movement Studio is to combine dance, pilates, and occupational therapy in order to promote the benefits of movement in an accessible way. </p>
-            <hr />
-            <p><strong>Caitlin Elmslie, MA BSc BA <br />
+        <div className="col s12 l6 offset-l3">
+          <p>The goal of Works in Progress Movement Studio is to combine dance, pilates, and occupational therapy in order to promote the benefits of movement in an accessible way. </p>
+          <hr />
+          <p><strong>Caitlin Elmslie, MA BSc BA <br />
             Movement Educator <br />
             Certified Personal Trainer (CanFitPro) <br />
             Certified Pilates Mat Instructor (Body Harmonics)
             </strong></p>
 
-          </div>
         </div>
       </div>
     </div>
