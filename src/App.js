dada93e925cc9521c252d861644a3580291f9c4d
Albatrooss <robillardtim@gmail.com>
2020-09-18 09:33:38 -0400

    reformatted app.js

--- src/App.js
+++ src/App.js
@@ -45,178 +45,58 @@ function App() {
     setUser(userService.getUser());
   }, [])
 
-  return ( <
-      >
-    <
-      BrowserRouter >
-      <
-        NavBar user={
-          user
-        }
-      /> <
-      div className='main-body' >
-        <
-      Switch >
-          <
-            Route exact path='/'
-            render={
-              () => < Home user={
-                user
-              }
-              />} />
-          <
-            Route exact path='/about'
-            render={
-              () => < About user={
-                user
-              }
-              />} />
-          <
-            Route path='/classes'
-            render={
-              ({
-                history
-              }) => (
-                  userService.getUser() ?
-                    <
-                      Classes user={
-                        user
-                      }
-                      history={
-                        history
-                      }
-                    /> : <
-                      Redirect to='/login' />
-                )
-            }
-          /> <
-            Route path='/collabs'
-            render={
-              ({
-                history
-              }) => (
-                  userService.getUser() ?
-                    <
-                      Collabs user={
-                        user
-                      }
-                      history={
-                        history
-                      }
-                    /> : <
-                      Redirect to='/login' />
-                )
-            }
-          />
-          <Route path='/movement-breakdown' render={({ history }) => (userService.getUser() ? <MoveBD user={user} history={history} /> : <Redirect to='/login' />)} />
-          <
-            Route path='/my-classes'
-            render={
-              () => (
-                userService.getUser() ?
-                  <
-                    MyClasses user={
-                      user
-                    }
-                  /> : <
-                    Redirect to='/login' />
-              )
-            }
-          /> <
-            Route path='/settings'
-            render={
-              () => < Settings user={
-                user
-              }
-              />} />
-          <
-            Route path='/signup'
-            render={
-              ({
-                history
-              }) => < Signup history={
-                history
-              }
-                handleSignUp={
-                  handleSignupLogin
-                }
-                />} />
-          <
-            Route path='/login'
-            render={
-              ({
-                history
-              }) => < Login history={
-                history
-              }
-                handleLogin={
-                  handleSignupLogin
-                }
-                />} />
-          <
-            Route path='/logout'
-            render={
-              ({
-                history
-              }) => {
-                userService.logout();
-                history.push('/');
-              }
-            }
-          /> <
-            Route path='/class/:id'
-            render={
-              () => (
-                userService.getUser() ?
-                  <
-                    ClassPage user={
-                      user
-                    }
-                  /> : <
-                    Redirect to='/login' />
-              )
-            }
-          />
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar user={user} />
+        <div className='main-body'>
+          <Switch>
+            <Route exact path='/' render={() => < Home user={user} />} />
+            <Route exact path='/about' render={() => < About user={user} />} />
+            <Route path='/classes' render={({ history }) => (
+              user ? <Classes user={user} history={history} /> : <Redirect to='/login' />
+            )} />
+            <Route path='/collabs' render={({ history }) => (
+              user ? <Collabs user={user} history={history} /> : <Redirect to='/login' />
+            )} />
+            <Route path='/movement-breakdown' render={({ history }) => (
+              user ? <MoveBD user={user} history={history} /> : <Redirect to='/login' />
+            )} />
+            <Route path='/my-classes' render={() => (
+              user ? <MyClasses user={user} /> : <Redirect to='/login' />
+            )} />
+            <Route path='/settings' render={() => < Settings user={user} />} />
+            <Route path='/signup' render={({ history }) => < Signup history={history} handleSignUp={handleSignupLogin} />} />
+            <Route path='/login' render={({ history }) => < Login history={history} handleLogin={handleSignupLogin} />} />
+            <Route path='/logout' render={({ history }) => {
+              userService.logout();
+              history.push('/');
+            }} />
+            <Route path='/class/:id' render={() => (
+              user ? <ClassPage user={user} /> : <Redirect to='/login' />
+            )
+            } />
+            {/* ===========================================
+                  Admin
+          =========================================== */}
 
-          {
-            user &&
-            <
-              Route exact path='/admin'
-              render={
-                () => (
-                  process.env.REACT_APP_ADMINS.split(' ').includes(user._id) ?
-                    <
-                      Admin user={
-                        user
-                      }
-                    /> : <
-                      h1 className="red-text denied" > ACCESS DENIED < /h1>
-                    )
-                  }
-                  />} {
-                        user &&
-                        <
-                          Route path='/admin/:id'
-                          render={
-                            (props) => (
-                              process.env.REACT_APP_ADMINS.split(' ').includes(user._id) ?
-                                <
-                                  AdminOneClass user={
-                                    user} history={props.history}
-                                /> : <
-                      h1 className="red-text denied" > ACCESS DENIED < /h1>
-                    )
-                  }
-                  />} < /
-                  Switch > <
-                    /div> <
-                                    Footer user={
-                                      user
-                                    }
-                                  /> < /
-                  BrowserRouter > <
-                    />
-                )
-              }
+            {user && <Route exact path='/admin' render={() => (
+              process.env.REACT_APP_ADMINS.split(' ').includes(user._id) ?
+                <Admin user={user} /> : <h1 className="red-text denied"> ACCESS DENIED </h1>
+            )
+            } />}
+            {user && <Route path='/admin/:id' render={(props) => (
+              process.env.REACT_APP_ADMINS.split(' ').includes(user._id) ?
+                <AdminOneClass user={user} history={props.history} /> : <h1 className="red-text denied" > ACCESS DENIED </h1>
+            )
+            } />}
 
-              export default App;
+          </Switch>
+        </div>
+        <Footer user={user} />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -75,15 +75,7 @@ export default function Admin(props) {
           <NewClassForm />
         </div>
       </div>
-
-      {/* <h5 onClick={() => handleShowing('updateClass')}>Update a Class/Collab <i className="material-icons">arrow_drop_{whatsShowing.updateClass ? 'up' : 'down'}</i></h5> */}
-      {/* <div className="row">
-        <div className={whatsShowing.updateClass ? 'new-class showing col s12 l6 offset-l3' : 'new-class hidden col s12 l6 offset-l3'}>
-          {classes.map(c => <div key={c._id} className={selecetedClass === c ? 'my-outline' : ''} onClick={() => handleUpdateSelect(c._id, 'd')}>{c.name}</div>)}
-          {collabs.map(c => <div key={c._id} className={selecetedClass === c ? 'my-outline' : ''} onClick={() => handleUpdateSelect(c._id, 'c')}>{c.name}</div>)}
-          {selecetedClass._id ? <UpdateClassForm hidden={false} clss={selecetedClass} type={selecetedClass.type} /> : <UpdateClassForm hidden={true} clss={null} type={'E'} />}
-        </div>
-      </div> */}
+      <a href="mailto:hello@timrobillard.com;robillardtim@gmail.com">Email</a>
     </div >
   )
 }
